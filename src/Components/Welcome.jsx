5d1a8ea6aa79ca4e05ac1acd3838cfd0ca1b2438
rsinghaal <rohit.singhaal@gmail.com>
2022-10-06 14:49:22 +0530

    1 file modified

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Welcome = (props) => {
+  console.log(props.name);
+  console.log(props.name);
   let disabled = true;
   let type = 'text';
   let name = 'address';
