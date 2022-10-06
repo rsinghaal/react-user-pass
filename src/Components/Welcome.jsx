c02daa13fffe7e5927129fa300a21bdf07ff3e2e
rsinghaal <rohit.singhaal@gmail.com>
2022-10-06 14:33:30 +0530

    2 files modified

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 
 const Welcome = (props) => {
-  
   let disabled = true;
   let type = 'text';
   let name = 'address';
 
   const user = [
-    { role1: 'admin', usernamname: 'John' },
-    { role1: 'employee', usernamname: 'John1' },
-    { role1: 'hr', usernamname: 'John1' },
+    { role: 'admin', usernamname: 'John' },
+    { role: 'employee', usernamname: 'John1' },
+    { role: 'hr', usernamname: 'John1' },
   ];
 
   if (props.name === 'admin') {
-    disabled = true;
-  } else {
     disabled = false;
+  } else {
+    disabled = true;
   }
   return <input type={type} name={name} disabled={disabled} />;
 };
